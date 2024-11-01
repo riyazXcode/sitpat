f0b9c935ddd048314c02d3068f92c518e8b7eb43
riyazXcode <riyazkhanprivate.nex@gmail.com>
2024-11-01 18:33:13 +0530

    changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,8 @@ import { CircleUser } from 'lucide-react';
 
 const Header = () => {
     return (
-        <div className='block bg-main-cyan-green h-[8%]'>
-            <div className='flex flex-row-reverse h-full w-full pr-2'>
+        <div className='block bg-main-cyan-green h-[8%] float-right'>
+            <div className='flex flex-row-reverse h-full pr-2'>
                 <button className='right-0'>
                     <CircleUser size={40} color='white' b/>
                 </button>
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -17,7 +17,7 @@ const SideBar = () => {
   }
 
   return (
-      <aside className={`h-screen ${isCollapsed ? 'w-14' : 'w-64'} transition-all duration-200`}>
+      <aside className={`h-screen ${isCollapsed ? 'w-12' : 'w-64'} transition-all duration-200`}>
         <nav className="h-full flex flex-col border-r shadow-sm bg-bg-grey">
           <div className="pt-4 px-2 pb-2 flex justify-between items-center drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]">
             {!isCollapsed && <img src={LogoPNG} className="w-28 transition-all duration-200" alt="Logo" />}
